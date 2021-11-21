b36b0ca431d6eaec0367feed801434b91a4fc61f
David Brutoczki <bcz.david99@gmail.com>
2021-11-21 19:36:59 +0100

    add static config variables

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,11 @@ import { homedir } from "os";
 import { existsSync, mkdirSync } from "fs";
 
 export class Config {
+	public static readonly IV_LENGTH: number = 16;
+	public static readonly ALGORITHM: string = "aes-256-ctr";
+	public static readonly SALT_LENGTH: number = 16;
+	public static readonly KEY_LENGTH: number = 32;
+
 	public static get PATH(): string {
 		const path: string = homedir() + "/.credman";
 
